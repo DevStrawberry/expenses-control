021f8f9b61feb8b5c020ae93a4b6718c93c53eb3
DevStrawberry <matheustrindade05@outlook.com>
2025-03-21 22:32:26 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBDnwoHigSl-GojlRXR-53yCfmrAiGONE",
+    authDomain: "controle-de-gastos-11739.firebaseapp.com",
+    projectId: "controle-de-gastos-11739",
+    storageBucket: "controle-de-gastos-11739.firebasestorage.app",
+    messagingSenderId: "749353372468",
+    appId: "1:749353372468:web:f31262516dc7f5bc8e7361"
+};
+firebase.initializeApp(firebaseConfig);
